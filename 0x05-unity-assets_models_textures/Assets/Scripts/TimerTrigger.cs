4f89b7104728241bff1825721cef2a2430426a54
BrandynR <brandynr@github.com>
2020-06-06 22:32:04 -0400

    First push

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,26 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>Triggers the timer when needed.</summary>
+public class TimerTrigger : MonoBehaviour
+{
+    /// <summary>Start the timer when the player moves.</summary>
+    private void OnTriggerExit(Collider other) {
+        if (other.name.Equals("Player"))
+            other.GetComponent<Timer>().enabled = true;
+    }
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnTriggerExit(Collider other)
+    {
+        player.GetComponent<Timer>().enabled = true;
+    }
+}
