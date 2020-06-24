e35bb1c75de931c749c812a6fa69cd516b98284f
BrandynR <brandynr@github.com>
2020-06-24 14:21:11 -0400

    ADD Invert y axis

--- 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour {
 
 	int backScene;
-
+    public Toggle inverted;
 	public void Back()
 	{
 		SceneManager.LoadScene(backScene);
 	}
 
 	public void SetScene(int prevScene)
+	{
+		backScene = prevScene;
+	}
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public Toggle inverted;
+    int backScene;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetString("IsInverted") != "")
+            if (bool.Parse(PlayerPrefs.GetString("IsInverted")) != inverted.isOn)
+                inverted.isOn = bool.Parse(PlayerPrefs.GetString("IsInverted"));
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetString("PreviousScene"));
+    }
+
+    public void Apply()
+    {
+        PlayerPrefs.SetString("IsInverted", inverted.isOn.ToString());
+    }
+    public void SetScene(int prevScene)
 	{
 		backScene = prevScene;
 	}
