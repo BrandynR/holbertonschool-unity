6f658b9c1f641f9ccb227aacc5cb119a3f50524f
BrandynR <brandynr@github.com>
2020-07-06 23:38:46 -0400

    FIX camera controller

--- 0x07-unity-animation/Assets/Scripts/CameraController.cs
+++ 0x07-unity-animation/Assets/Scripts/CameraController.cs
@@ -1,51 +1,4 @@
-﻿/*using UnityEngine;
-
-///<summary>Handles camera control, allowing player to rotate the camera with the mouse</summary>
-public class CameraController : MonoBehaviour
-{
-    public float turn = 3f;
-    public Transform player;
-    private Transform tsfrm;
-    private Vector3 offset;
-    private Quaternion turnX;
-    private Quaternion turnY;
-
-    public bool isInverted;
-    private int inverted;
-
-    void Awake()
-    {
-        tsfrm = GetComponent<Transform>();
-    }
-
-    void Start()
-    {   
-        Cursor.visible = false;
-        offset = new Vector3(0, 1.25f, -6.25f);
-        if (PlayerPrefs.GetString("IsInverted") != "")
-            isInverted = bool.Parse(PlayerPrefs.GetString("IsInverted"));
-        else
-            isInverted = false;
-    }
-
-    void LateUpdate()
-    {
-        if (isInverted)
-        {
-            inverted = -1;
-        }
-        else
-        {
-            inverted = 1;
-        }
-        turnX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turn, Vector3.up);
-        turnY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turn, Vector3.left);
-        offset = turnX * turnY * offset;
-        transform.position = player.position + offset;
-        transform.LookAt(player.position + new Vector3(0, 0.24f, 0));
-    }
-}*/
-using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -85,5 +38,61 @@ public class CameraController : MonoBehaviour
         offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * inverted * turnSpeed, Vector3.left) * offset;
         t.position = player.transform.position + offset * Time.timeScale;
         transform.LookAt(player.transform.position);
+    }
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Transform playerPos;
+    public Vector3 offset;
+    // public float speedH = 2.0f;
+    // public float speedV = 2.0f;
+    // private float x = 0.0f;
+    // private float y = 0.0f;
+    public bool rotateAround = true;
+    public float rotationSpeed = 5.0f;
+    public bool LookAtPlayer = false;
+    //public bool isInverted;
+    //public Toggle inverted;
+    //public float mouseY = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //inverted = this.GetComponent<Toggle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerPos.position + offset;
+        if (rotateAround)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                Quaternion camTurn = 
+                    Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+                
+                offset = camTurn * offset;
+            // x +=speedH * Input.GetAxis("Mouse X");
+            // y += speedV * Input.GetAxis("Mouse Y");
+            // transform.eulerAngles = new Vector3(y, x, 0.0f);
+            }
+        }
+        if (LookAtPlayer || rotateAround)
+            transform.LookAt(playerPos);
+
+    //     if (inverted.isOn)
+    //     {
+    //         isInverted = true;
+    //         transform.RotateAround(transform.position, transform.right, Input.GetAxis("Mouse Y") * mouseY);
+    //     }
+    //     else
+    //         isInverted = false;
     }
 }
