40a30fe33a879e076f81ee98271ae0c78f0645bb
BrandynR <brandynr@github.com>
2020-07-22 21:11:41 -0400

    FIX BGM in level 2+3

--- 0x08-unity-audio/Assets/Scripts/OptionsMenu.cs
+++ 0x08-unity-audio/Assets/Scripts/OptionsMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class OptionsMenu : MonoBehaviour
     public GameObject pauseCanvas;
     public AudioSource buttonRollover;
 	public AudioSource buttonClick;
+    public AudioMixer mixer;
 
     public void Back()
     {
@@ -21,7 +23,13 @@ public class OptionsMenu : MonoBehaviour
     public void Apply()
     {
         PlayerPrefs.SetString("IsInverted", inverted.isOn.ToString());
-        Back();
+        PlayerPrefs.SetFloat("BGMVol", GameObject.Find("BGMSlider").GetComponent<Slider>().value);
+        PlayerPrefs.SetFloat("SFXVol", GameObject.Find("SFXSlider").GetComponent<Slider>().value);
+        this.mixer.SetFloat("BGMVol", PlayerPrefs.GetFloat("BGMVol") != 0 ? 20 * Mathf.Log10(PlayerPrefs.GetFloat("BGMVol")) : -144);
+        this.mixer.SetFloat("RunningVol", PlayerPrefs.GetFloat("SFXVol") != 0 ? 20 * Mathf.Log10(PlayerPrefs.GetFloat("SFXVol")) : -144);
+        this.mixer.SetFloat("LandingVol", PlayerPrefs.GetFloat("SFXVol") != 0 ? 20 * Mathf.Log10(PlayerPrefs.GetFloat("SFXVol")) : -144);
+        this.mixer.SetFloat("AmbientVol", PlayerPrefs.GetFloat("SFXVol") != 0 ? 20 * Mathf.Log10(PlayerPrefs.GetFloat("SFXVol")) : -144);
+        //Back();
     }
     public void SetScene(int prevScene)
 	{
@@ -35,4 +43,18 @@ public class OptionsMenu : MonoBehaviour
 	{
 		buttonClick.Play();
 	}
+    private void Start() {
+        if (PlayerPrefs.HasKey("InvertY"))
+            this.GetComponentInChildren<Toggle>().isOn = PlayerPrefs.GetInt("InvertY") == 0 ? false : true;
+        else
+            this.GetComponentInChildren<Toggle>().isOn = false;
+        if (PlayerPrefs.HasKey("BGMVol"))
+            GameObject.Find("BGMSlider").GetComponent<Slider>().value = PlayerPrefs.GetFloat("BGMVol");
+        else
+            GameObject.Find("BGMSlider").GetComponent<Slider>().value = 1;
+        if (PlayerPrefs.HasKey("SFXVol"))
+            GameObject.Find("SFXSlider").GetComponent<Slider>().value = PlayerPrefs.GetFloat("SFXVol");
+        else
+            GameObject.Find("SFXSlider").GetComponent<Slider>().value = 1;
+    }
 }
