7ecf5d7c086237e2a744d55c12bbf880a2511df4
BrandynR <brandynr@github.com>
2020-09-15 20:08:01 -0400

    Enabled projector more effieciently

---
+++ 0x0B-unity-vr_room/Assets/Scripts/ProjectorPlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectorPlay : MonoBehaviour
+{
+    //public ParticleSystem particles;
+    public GameObject particleSystem;
+    //bool hologram;
+
+    public void ProjectorOn()
+    {
+        if (particleSystem.active)
+        {
+            particleSystem.SetActive(false);
+        }
+        else
+        {
+            particleSystem.SetActive(true);
+        }
+        //particles.Play();
+    }
+
+}
