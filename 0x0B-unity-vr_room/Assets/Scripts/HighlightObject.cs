4fdc99821b05e8cbb7068f902b8d7da067fe576c
BrandynR <brandynr@github.com>
2020-09-16 23:19:31 -0400

    ADD Highlight to interactable surfaces

---
+++ 0x0B-unity-vr_room/Assets/Scripts/HighlightObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightObject : MonoBehaviour
+{
+    public GameObject interactable;
+   // public Transform grab;
+ void OnMouseEnter()
+ {
+     interactable.GetComponent<Outline>().enabled = true;
+ }
+ void OnMouseExit()
+ {
+     interactable.GetComponent<Outline>().enabled = false;
+ }
+/*void OnMouseBottonDown()
+{
+    interactable.transform.position = grab.transform.position;
+}*/
+
+}
